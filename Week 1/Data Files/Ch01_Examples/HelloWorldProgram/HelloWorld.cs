8b117a09c4b7eb55337fd6542009c2809765693b
MorreseMorrison <rese298@gmail.com>
2023-03-25 22:32:21 -0400

    Add files via upload

---
+++ Week 1/Data Files/Ch01_Examples/HelloWorldProgram/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿// This is traditionally the first program written.
+using System;
+using static System.Console;
+
+namespace HelloWorldProgram
+{
+    class HelloWorld
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = 99;
+            int c;
+
+            c = a + b;
+            WriteLine(c);
+            ReadKey();
+        }
+    }
+}
